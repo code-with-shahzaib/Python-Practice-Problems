d630680595552962e26b77423949500a88b2cc2f
Muhammad Shahzaib <codewithshahzaib@gmail.com>
2026-05-29 12:32:12 +0700

    Add Third Problem Solution

---
+++ Practice Set 01/use_of_an_external_module.py
@@ -0,0 +1,9 @@
+"""
+3. Install an external module and use it to perform an operation of your interest. 
+"""
+
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.say("I'm on the way, but I don't want to reach")
+engine.runAndWait()
