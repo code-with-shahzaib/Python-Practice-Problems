aafab2160a3f7aaf15953a1399dfcf932e75bf64
Muhammad Shahzaib <codewithshahzaib@gmail.com>
2026-06-04 18:29:01 +0700

    Add 2nd Problem Solution

---
+++ Practice Set 02/find_reminder.py
@@ -0,0 +1,10 @@
+"""
+2. Write a python program to find remainder when a number is divided by z.
+"""
+
+a = int(input("Enter First Number: "))
+b = int(input("Enter Second Number: "))
+
+reminder = a % b
+
+print(f"The Reminder of {a} % {b} = {reminder}")
