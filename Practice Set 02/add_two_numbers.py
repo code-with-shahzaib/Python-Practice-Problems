d49962adb9001674dbb0c67c7efc3a064e1524a5
Muhammad Shahzaib <codewithshahzaib@gmail.com>
2026-06-04 18:25:11 +0700

    Add 1st Promlem Solution

---
+++ Practice Set 02/add_two_numbers.py
@@ -0,0 +1,10 @@
+"""
+1. Write a python program to add two numbers.
+"""
+
+num_1 = int(input("Enter First Number: "))
+num_2 = int(input("Enter First Second: "))
+
+addition = num_1 + num_2
+
+print(f"The Sum of {num_1} + {num_2} = {addition}")
