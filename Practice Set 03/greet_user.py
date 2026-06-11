c5758e23e3a368e572e9f7be75edd5f51e1208e3
Muhammad Shahzaib <codewithshahzaib@gmail.com>
2026-06-11 21:09:00 +0700

    Add 1st Problem Solution

---
+++ Practice Set 03/greet_user.py
@@ -0,0 +1,7 @@
+"""1. Write a python program to display a user entered name followed by Good
+Afternoon using input () function.
+"""
+
+user_name = input("What's Your Name Sir? ") # I designed this script only for male so I used Sir word. Don't replace it.
+
+print(f"Good Afternoon {user_name} Sir!")
