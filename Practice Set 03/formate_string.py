75c6fc4e5c3da3e39e311a1af02f425b433db1ec
Muhammad Shahzaib <codewithshahzaib@gmail.com>
2026-06-15 22:09:11 +0700

    Add 5th Problem Solution

---
+++ Practice Set 03/formate_string.py
@@ -0,0 +1,8 @@
+"""
+5. Write a program to format the following letter using escape sequence
+characters.
+letter = "Dear Harry, this python course is nice. Thanks!"
+"""
+
+letter = "Dear Harry,\nthis python course is nice.\n\tThanks!"
+print(letter)
